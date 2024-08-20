9f68197d1fe501361c2e18fdd60f004a763818ad
Михаил Пигалов <pigalovmihail@yandex.ru>
2024-08-20 08:54:20 +0300

    правки

--- ReactProject/src/main.jsx
+++ ReactProject/src/main.jsx
@@ -2,31 +2,10 @@ import { createElement } from "react";
 import { createRoot } from "react-dom/client";
 import { restaurants } from "../materials/mock.js";
 
-const data = restaurants;
-
-const firstRestaurantData = data[0];
-const secondRestaurantData = data[1];
-const thirdRestaurantData = data[2];
-const fourRestaurantData = data[3];
-
-const firstRestaurantElement = createElementByTemplate(firstRestaurantData);
-const secondRestaurantElement = createElementByTemplate(secondRestaurantData);
-const thirdRestaurantElement = createElementByTemplate(thirdRestaurantData);
-const fourRestaurantElement = createElementByTemplate(fourRestaurantData);
-
-const allItemsDiv = createElement(
-  "div",
-  {},
-  firstRestaurantElement,
-  secondRestaurantElement,
-  thirdRestaurantElement,
-  fourRestaurantElement
-);
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(allItemsDiv);
+root.render(restaurants.map(r => createElementByTemplate(r)));
 
 function createElementByTemplate(restaurantData) {
   const styles = { border: "1px solid black", margin: "10px", padding: "10px", borderTopLeftRadius: "10px", borderTopRightRadius: "10px", borderBottomRightRadius: "10px", borderBottomLeftRadius: "10px"};
